8bbb9faf624a5f67cd76e66bbb0879c617f62a92
DiegoVega19 <davila9923@gmail.com>
2023-01-03 17:47:26 -0600

    last changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, Container } from "@mui/material";
 import { Navbar } from "./common/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./routes/router";
+import { NotificationProvider } from "./context/notification.context";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRouter></AppRouter>
-    </BrowserRouter>
+    <NotificationProvider>
+      <BrowserRouter>
+        <Suspense fallback={"Cargando..."}>
+          <AppRouter></AppRouter>
+        </Suspense>
+      </BrowserRouter>
+    </NotificationProvider>
   );
 }
 
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { Notification } from './Notifications';
+export {Header} from "./Header"
+export { CardComponent } from "./Card"
